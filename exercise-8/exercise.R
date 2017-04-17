9c6b4f77ec31379e2a1c967853757792c8a7ec90
andyblueyo <abchen@uw.edu>
2017-04-17 03:02:55 -0700

    delete answers

--- exercise-8/exercise.R
+++ exercise-8/exercise.R
@@ -2,3 +2,34 @@
 
 # Read in the data
 pulitzer <- read.csv("data/pulitzer-circulation-data.csv", stringsAsFactors = FALSE)
+
+# Install and load the needed libraries
+# Be sure to comment out the install.packages function so it won't install it every time it runs
+# Remeber you only need to install a package once
+#install.packages(dplyr)
+library(dplyr)
+
+# View in the data set. Start to understand what the data columns contains
+# Be sure to comment out the function so it won't view everytime you run the code.
+
+
+# Use 'colnames' to print out the names of the columns
+
+
+# Use 'str' to print what types of values are contained in each column
+# Did any value type surprise you? Why do you think they are that type?
+
+
+# Add a column that contains the diffrence in changes in Daily Circulation from 2004 to 2013
+
+
+# What publication gained the most pulitzer prizes from 2004-2014?
+# Be sure to use the pipe operator! 
+
+
+# Which publication with at least 5 Pulitzers won from 2004-2014 had the biggest decrease(negative) in Daily circulation numbers? 
+# This publication should have Pulitzer prizes won a minimum of 5 Pulitzers, as well as the biggest decrease in circulation
+
+
+# Your turn! An important part about being a data scientist is asking questions. 
+# Create a question and use dplyr to figure out the answer.  
