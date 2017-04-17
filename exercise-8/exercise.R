7cbd2a17302bc37379dbe39b90dd9802b2d2cdf6
andyblueyo <abchen@uw.edu>
2017-04-17 03:01:29 -0700

    finish complete

--- exercise-8/exercise.R
+++ exercise-8/exercise.R
@@ -2,3 +2,43 @@
 
 # Read in the data
 pulitzer <- read.csv("data/pulitzer-circulation-data.csv", stringsAsFactors = FALSE)
+
+# Install and load the needed libraries
+# Be sure to comment out the install.packages function so it won't install it every time it runs
+# Remeber you only need to install a package once
+#install.packages(dplyr)
+library(dplyr)
+
+# View in the data set. Start to understand what the data columns contains
+# Be sure to comment out the function so it won't view everytime you run the code.
+# View(pulitzer)
+
+# Use 'colnames' to print out the names of the columns
+print(colnames(pulitzer))
+
+# Use 'str' to print what types of values are contained in each column
+# Did any value type surprise you? WHy do you think they are that type?
+print(str(pulitzer))
+
+# Add a column that contains the diffrence in changes in Daily Circulation from 2004 to 2013
+pulitzer.differnce <- mutate(pulitzer, diff.2004.2014.to.1990.2003 = 
+                            Pulitzer.Prize.Winners.and.Finalists..2004.2014 - 
+                            Pulitzer.Prize.Winners.and.Finalists..1990.2003)
+
+# What publication gained the most pulitzer prizes from 2004-2014?
+# Be sure to use the pipe operator! 
+most.2014 <- filter(pulitzer.differnce, max(Pulitzer.Prize.Winners.and.Finalists..2004.2014) == 
+                    Pulitzer.Prize.Winners.and.Finalists..2004.2014) %>% 
+              select(Newspaper)
+
+# Which publication with at least 5 Pulitzers won from 2004-2014 had the biggest decrease(negative) in Daily circulation numbers? 
+# This publication should have Pulitzer prizes won a minimum of 5 Pulitzers, as well as the biggest decrease in circulation
+most.award.winning.decrease <- filter(pulitzer.differnce, Pulitzer.Prize.Winners.and.Finalists..2004.2014 >= 5) %>% 
+                                filter(min(Change.in.Daily.Circulation..2004.2013 ) == Change.in.Daily.Circulation..2004.2013) %>% 
+                                select(Newspaper)
+
+# Your turn! An important part about being a data scientist is asking questions. 
+# Create a question and use dplyr to figure out the answer.  
+
+  
+
