38dcd18c978ccafa1feb43df35c9476ac6565e87
Christy Lu <hyl9@uw.edu>
2017-04-19 14:38:34 -0700

    lab

--- exercise-8/exercise.R
+++ exercise-8/exercise.R
@@ -11,26 +11,29 @@ library(dplyr)
 
 # View in the data set. Start to understand what the data columns contains
 # Be sure to comment out the function so it won't view everytime you run the code.
-
+View(pulitzer)
 
 # Use 'colnames' to print out the names of the columns
-
+colnames(pulitzer)
 
 # Use 'str' to print what types of values are contained in each column
 # Did any value type surprise you? Why do you think they are that type?
-
+str(col(pulitzer))
 
 # Add a column in a dataframe called 'Pulitzer.Prize.Change` that contains the diffrence in changes
 # in Pulitzer Prize Winners from 2004 to 2013 and Pultizer Prize Winners from 1990 to 2003.
-
+pultizer <- mutate(pulitzer, Pulitzer.Prize.Change = Pulitzer.Prize.Winners.and.Finalists..1990.2014 - Pulitzer.Prize.Winners.and.Finalists..1990.2003)
 
 # What publication gained the most pulitzer prizes from 2004-2014?
 # Be sure to use the pipe operator! 
-
+most.pulitzer <- select(pulitzer, Newspaper, Pulitzer.Prize.Winners.and.Finalists..2004.2014) %>% 
+  arrange(-Pulitzer.Prize.Winners.and.Finalists..2004.2014)
 
 # Which publication with at least 5 Pulitzers won from 2004-2014 had the biggest decrease(negative) in Daily circulation numbers? 
 # This publication should have Pulitzer prizes won a minimum of 5 Pulitzers, as well as the biggest decrease in circulation
-
+select(pulitzer, Newspaper, Pulitzer.Prize.Winners.and.Finalists..2004.2014, Change.in.Daily.Circulation..2004.2013) %>% 
+  filter(Pulitzer.Prize.Winners.and.Finalists..2004.2014 >= 5) %>% 
+  filter(Change.in.Daily.Circulation..2004.2013 == min(Change.in.Daily.Circulation..2004.2013))
 
 # Your turn! An important part about being a data scientist is asking questions. 
-# Create a question and use dplyr to figure out the answer.  
+# Create a question and use dplyr to figure out the answer. 
